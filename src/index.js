918bff896c5a98b5183c4d191a29522b38c8c5a5
袁广龙 <yuanguanglong@popmart.com>
2022-06-25 14:54:09 +0800

    feat: 升级依赖文件，调通项目

--- src/index.js
+++ src/index.js
@@ -13,123 +13,124 @@ import components from './components';
 import panels from './panels';
 import styles from './styles';
 
-export default grapesjs.plugins.add('gjs-preset-webpage', (editor, opts = {}) => {
-  let config = opts;
+export default grapesjs.plugins.add(
+  'gjs-preset-popmart',
+  (editor, opts = {}) => {
+    let config = opts;
 
-  let defaults = {
-    // Which blocks to add
-    blocks: ['link-block', 'quote', 'text-basic'],
+    let defaults = {
+      // Which blocks to add
+      blocks: ['link-block', 'quote', 'text-basic'],
 
-    // Modal import title
-    modalImportTitle: 'Import',
+      // Modal import title
+      modalImportTitle: 'Import',
 
-    // Modal import button text
-    modalImportButton: 'Import',
+      // Modal import button text
+      modalImportButton: 'Import',
 
-    // Import description inside import modal
-    modalImportLabel: '',
+      // Import description inside import modal
+      modalImportLabel: '',
 
-    // Default content to setup on import model open.
-    // Could also be a function with a dynamic content return (must be a string)
-    // eg. modalImportContent: editor => editor.getHtml(),
-    modalImportContent: '',
+      // Default content to setup on import model open.
+      // Could also be a function with a dynamic content return (must be a string)
+      // eg. modalImportContent: editor => editor.getHtml(),
+      modalImportContent: '',
 
-    // Code viewer (eg. CodeMirror) options
-    importViewerOptions: {},
+      // Code viewer (eg. CodeMirror) options
+      importViewerOptions: {},
 
-    // Confirm text before cleaning the canvas
-    textCleanCanvas: 'Are you sure to clean the canvas?',
+      // Confirm text before cleaning the canvas
+      textCleanCanvas: 'Are you sure to clean the canvas?',
 
-    // Show the Style Manager on component change
-    showStylesOnChange: 1,
+      // Show the Style Manager on component change
+      showStylesOnChange: 1,
 
-    // Text for General sector in Style Manager
-    textGeneral: 'General',
+      // Text for General sector in Style Manager
+      textGeneral: 'General',
 
-    // Text for Layout sector in Style Manager
-    textLayout: 'Layout',
+      // Text for Layout sector in Style Manager
+      textLayout: 'Layout',
 
-    // Text for Typography sector in Style Manager
-    textTypography: 'Typography',
+      // Text for Typography sector in Style Manager
+      textTypography: 'Typography',
 
-    // Text for Decorations sector in Style Manager
-    textDecorations: 'Decorations',
+      // Text for Decorations sector in Style Manager
+      textDecorations: 'Decorations',
 
-    // Text for Extra sector in Style Manager
-    textExtra: 'Extra',
+      // Text for Extra sector in Style Manager
+      textExtra: 'Extra111',
 
-    // Use custom set of sectors for the Style Manager
-    customStyleManager: [],
+      // Use custom set of sectors for the Style Manager
+      customStyleManager: [],
 
-    // `grapesjs-blocks-basic` plugin options
-    // By setting this option to `false` will avoid loading the plugin
-    blocksBasicOpts: {},
+      // `grapesjs-blocks-basic` plugin options
+      // By setting this option to `false` will avoid loading the plugin
+      blocksBasicOpts: {},
 
-    // `grapesjs-navbar` plugin options
-    // By setting this option to `false` will avoid loading the plugin
-    navbarOpts: {},
+      // `grapesjs-navbar` plugin options
+      // By setting this option to `false` will avoid loading the plugin
+      navbarOpts: {},
 
-    // `grapesjs-component-countdown` plugin options
-    // By setting this option to `false` will avoid loading the plugin
-    countdownOpts: {},
+      // `grapesjs-component-countdown` plugin options
+      // By setting this option to `false` will avoid loading the plugin
+      countdownOpts: {},
 
-    // `grapesjs-plugin-forms` plugin options
-    // By setting this option to `false` will avoid loading the plugin
-    formsOpts: {},
+      // `grapesjs-plugin-forms` plugin options
+      // By setting this option to `false` will avoid loading the plugin
+      formsOpts: {},
 
-    // `grapesjs-plugin-export` plugin options
-    // By setting this option to `false` will avoid loading the plugin
-    exportOpts: {},
+      // `grapesjs-plugin-export` plugin options
+      // By setting this option to `false` will avoid loading the plugin
+      exportOpts: {},
 
-    // `grapesjs-aviary` plugin options, disabled by default
-    // Aviary library should be included manually
-    // By setting this option to `false` will avoid loading the plugin
-    aviaryOpts: 0,
+      // `grapesjs-aviary` plugin options, disabled by default
+      // Aviary library should be included manually
+      // By setting this option to `false` will avoid loading the plugin
+      aviaryOpts: 0,
 
-    // `grapesjs-plugin-filestack` plugin options, disabled by default
-    // Filestack library should be included manually
-    // By setting this option to `false` will avoid loading the plugin
-    filestackOpts: 0,
-  };
+      // `grapesjs-plugin-filestack` plugin options, disabled by default
+      // Filestack library should be included manually
+      // By setting this option to `false` will avoid loading the plugin
+      filestackOpts: 0,
+    };
 
-  // Load defaults
-  for (let name in defaults) {
-    if (!(name in config))
-      config[name] = defaults[name];
-  }
+    // Load defaults
+    for (let name in defaults) {
+      if (!(name in config)) config[name] = defaults[name];
+    }
 
-  const {
-    blocksBasicOpts,
-    navbarOpts,
-    countdownOpts,
-    formsOpts,
-    exportOpts,
-    aviaryOpts,
-    filestackOpts
-  } = config;
+    const {
+      blocksBasicOpts,
+      navbarOpts,
+      countdownOpts,
+      formsOpts,
+      exportOpts,
+      aviaryOpts,
+      filestackOpts,
+    } = config;
 
-  // Load plugins
-  blocksBasicOpts && pluginBlocks(editor, blocksBasicOpts);
-  navbarOpts && pluginNavbar(editor, navbarOpts);
-  countdownOpts && pluginCountdown(editor, countdownOpts);
-  formsOpts && pluginForms(editor, formsOpts);
-  exportOpts && pluginExport(editor, exportOpts);
-  aviaryOpts && pluginAviary(editor, aviaryOpts);
-  filestackOpts && pluginFilestack(editor, filestackOpts);
+    // Load plugins
+    blocksBasicOpts && pluginBlocks(editor, blocksBasicOpts);
+    navbarOpts && pluginNavbar(editor, navbarOpts);
+    countdownOpts && pluginCountdown(editor, countdownOpts);
+    formsOpts && pluginForms(editor, formsOpts);
+    exportOpts && pluginExport(editor, exportOpts);
+    aviaryOpts && pluginAviary(editor, aviaryOpts);
+    filestackOpts && pluginFilestack(editor, filestackOpts);
 
-  // Load components
-  components(editor, config);
+    // Load components
+    components(editor, config);
 
-  // Load blocks
-  blocks(editor, config);
+    // Load blocks
+    blocks(editor, config);
 
-  // Load commands
-  commands(editor, config);
+    // Load commands
+    commands(editor, config);
 
-  // Load panels
-  panels(editor, config);
+    // Load panels
+    panels(editor, config);
 
-  // Load styles
-  styles(editor, config);
-
-});
+    // Load styles
+    styles(editor, config);
+  },
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,35 +5,48 @@ const fs = require('fs');
 const name = pkg.name;
 let plugins = [];
 
-module.exports = (env = {}) => {
+module.exports = (env = {}, argv) => {
   if (env.production) {
     plugins = [
-      new webpack.optimize.UglifyJsPlugin({ minimize: true, compressor: { warnings: false }}),
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compressor: { warnings: false },
+      }),
       new webpack.BannerPlugin(`${name} - ${pkg.version}`),
-    ]
+    ];
   } else {
     const index = 'index.html';
     const indexDev = '_' + index;
-    plugins.push(new HtmlWebpackPlugin({
-      template: fs.existsSync(indexDev) ? indexDev : index
-    }));
+    plugins.push(
+      new HtmlWebpackPlugin({
+        template: fs.existsSync(indexDev) ? indexDev : index,
+      }),
+    );
   }
 
   return {
     entry: './src',
+    mode: argv.mode || 'none',
     output: {
-        filename: `./dist/${name}.min.js`,
-        library: name,
-        libraryTarget: 'umd',
+      filename: `./dist/${name}.min.js`,
+      library: name,
+      libraryTarget: 'umd',
     },
     module: {
-      loaders: [{
+      rules: [
+        {
           test: /\.js$/,
-          loader: 'babel-loader',
           include: /src/,
-      }],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     },
-    externals: {'grapesjs': 'grapesjs'},
+    externals: { grapesjs: 'grapesjs' },
     plugins: plugins,
   };
-}
+};
